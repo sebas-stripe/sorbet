b6aef2e9e613c5ee5e06a4982e130583a48601e8
Ariel Davis <ariel.z.davis@icloud.com>
2019-07-08 12:06:07 -0700

    Add more tests

--- gems/sorbet-runtime/test/types/final.rb
+++ gems/sorbet-runtime/test/types/final.rb
@@ -48,6 +48,74 @@ class Opus::Types::Test::FinalTest < Critic::Unit::UnitTest
     assert_includes(err.message, "was declared as final and cannot be redefined")
   end
 
+  it "forbids redefining a final instance method with a regular sig" do
+    err = assert_raises(RuntimeError) do
+      Class.new do
+        extend T::Sig
+        sig {void}.final
+        def foo; end
+        sig {void}
+        def foo; end
+      end
+    end
+    assert_includes(err.message, "was declared as final and cannot be redefined")
+  end
+
+  it "forbids redefining a final class method with a regular sig" do
+    err = assert_raises(RuntimeError) do
+      Class.new do
+        extend T::Sig
+        sig {void}.final
+        def self.foo; end
+        sig {void}
+        def self.foo; end
+      end
+    end
+    assert_includes(err.message, "was declared as final and cannot be redefined")
+  end
+
+  it "forbids redefining a final instance method with no sig" do
+    err = assert_raises(RuntimeError) do
+      Class.new do
+        extend T::Sig
+        sig {void}.final
+        def foo; end
+        def foo; end
+      end
+    end
+    assert_includes(err.message, "was declared as final and cannot be redefined")
+  end
+
+  it "forbids redefining a final class method with no sig" do
+    err = assert_raises(RuntimeError) do
+      Class.new do
+        extend T::Sig
+        sig {void}.final
+        def self.foo; end
+        def self.foo; end
+      end
+    end
+    assert_includes(err.message, "was declared as final and cannot be redefined")
+  end
+
+  it "allows redefining a regular instance method to be final" do
+    Class.new do
+      extend T::Sig
+      def foo; end
+      sig {void}.final
+      def foo; end
+    end
+  end
+
+  it "allows redefining a regular class method to be final" do
+    Class.new do
+      extend T::Sig
+      def self.foo; end
+      sig {void}.final
+      def self.foo; end
+    end
+  end
+
   it "forbids overriding a final instance method" do
     c = Class.new do
       extend T::Sig
@@ -71,6 +139,72 @@ class Opus::Types::Test::FinalTest < Critic::Unit::UnitTest
     err = assert_raises(RuntimeError) do
       Class.new(c) do
         def self.foo; end
+      end
+    end
+    assert_includes(err.message, "was declared as final and cannot be overridden")
+  end
+
+  it "forbids overriding a final method from an included module" do
+    m = Module.new do
+      extend T::Sig
+      sig {void}.final
+      def foo; end
+    end
+    err = assert_raises(RuntimeError) do
+      Class.new do
+        include m
+        def foo; end
+      end
+    end
+    assert_includes(err.message, "was declared as final and cannot be overridden")
+  end
+
+  it "forbids overriding a final method from an extended module" do
+    m = Module.new do
+      extend T::Sig
+      sig {void}.final
+      def foo; end
+    end
+    err = assert_raises(RuntimeError) do
+      Class.new do
+        extend m
+        def self.foo; end
+      end
+    end
+    assert_includes(err.message, "was declared as final and cannot be overridden")
+  end
+
+  it "forbids overriding a final method by including two modules" do
+    m1 = Module.new do
+      extend T::Sig
+      sig {void}.final
+      def foo; end
+    end
+    m2 = Module.new do
+      def foo; end
+    end
+    err = assert_raises(RuntimeError) do
+      Class.new do
+        include m1
+        include m2
+      end
+    end
+    assert_includes(err.message, "was declared as final and cannot be overridden")
+  end
+
+  it "forbids overriding a final method by extending two modules" do
+    m1 = Module.new do
+      extend T::Sig
+      sig {void}.final
+      def foo; end
+    end
+    m2 = Module.new do
+      def foo; end
+    end
+    err = assert_raises(RuntimeError) do
+      Class.new do
+        extend m1
+        extend m2
       end
     end
     assert_includes(err.message, "was declared as final and cannot be overridden")
